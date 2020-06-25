779e389d0f8889072cdbe7feead1ff720d2d1a49
Samuel Adjei <samueladj3i@gmail.com>
2020-06-25 22:36:50 +0100

    new commit

--- js/app.js
+++ js/app.js
@@ -1,31 +1,3 @@
-// const tabItems = document.querySelectorAll('.tab-item');
-// const tabContentItems = document.querySelectorAll('.tab-content-item');
-
-// // Select tab content item
-// function selectItem(e) {
-// 	// Remove all show and border classes
-// 	removeBorder();
-// 	removeShow();
-// 	// Add border to current tab
-// 	this.classList.add('tab-border');
-// 	// Grab content item from the DOM
-// 	const tabContentItem = document.querySelector(`#${this.id}-content`);
-// 	// Add show class
-// 	tabContentItem.classList.add('show');
-// }
-
-// // Remove bottom borders from all tab items
-// function removeBorder() {
-// 	tabItems.forEach((item) => item.classList.remove('tab-border'));
-// }
-
-// function removeShow() {
-// 	tabContentItems.forEach((item) => item.classList.remove('show'));
-// }
-
-// // Listen for tab click
-// tabItems.forEach((item) => item.addEventListener('click', selectItem));
-
 const tabItems = document.querySelectorAll('.tab-item');
 const tabContentItems = document.querySelectorAll('.tab-content-item');
 
@@ -34,9 +6,9 @@ function selectItem(e) {
 	// Remove all show and border classes
 	removeBorder();
 	removeShow();
-	// Add border to current tab item
+	// Add border to current tab
 	this.classList.add('tab-border');
-	// Grab content item from DOM
+	// Grab content item from the DOM
 	const tabContentItem = document.querySelector(`#${this.id}-content`);
 	// Add show class
 	tabContentItem.classList.add('show');
@@ -44,19 +16,23 @@ function selectItem(e) {
 
 // Remove bottom borders from all tab items
 function removeBorder() {
-	tabItems.forEach((item) => {
-		item.classList.remove('tab-border');
-	});
+	tabItems.forEach((item) => item.classList.remove('tab-border'));
 }
 
-// Remove show class from all content items
 function removeShow() {
-	tabContentItems.forEach((item) => {
-		item.classList.remove('show');
-	});
+	tabContentItems.forEach((item) => item.classList.remove('show'));
 }
 
-// Listen for tab item click
-tabItems.forEach((item) => {
-	item.addEventListener('click', selectItem);
+// Listen for tab click
+tabItems.forEach((item) => item.addEventListener('click', selectItem));
+
+// *TOGGLE
+const arrowToggle = document.querySelectorAll('.toggle');
+
+function toggleMe(e) {
+	console.log('Me');
+}
+
+arrowToggle.forEach((toggle) => {
+	toggle.addEventListener('click', toggleMe);
 });
