0c64ae84e273c209683eea0db1c463aa0a9974c3
Samuel Adjei <samueladj3i@gmail.com>
2020-06-27 22:54:50 +0100

    styling navigation

--- js/app.js
+++ js/app.js
@@ -36,3 +36,30 @@ function toggleMe(e) {
 arrowToggle.forEach((toggle) => {
 	toggle.addEventListener('click', toggleMe);
 });
+
+// Hamburger Menu 
+const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+// const fadeElements = document.querySelectorAll('.has-fade');
+const body = document.querySelector('body');
+
+btnHamburger.addEventListener('click', function () {
+
+  if (header.classList.contains('open')) { //Close hamburger menu
+    body.classList.remove('noscroll');
+    header.classList.remove('open');
+    // fadeElements.forEach(function (element) {
+    //   element.classList.remove('fade-in');
+    //   element.classList.add('fade-out');
+    // });
+
+  } else { //Open hamburger menu
+    body.classList.add('noscroll');
+    header.classList.add('open');
+    // fadeElements.forEach(function (element) {
+    //   element.classList.remove('fade-out');
+    //   element.classList.add('fade-in');
+    // });
+  }
+});
