2a8f9f1de67e0dbcb4b9cfdecea038c3f7815db6
Samuel Adjei <samueladj3i@gmail.com>
2020-06-27 23:34:15 +0100

    added mobile styling to hero

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 const tabItems = document.querySelectorAll('.tab-item');
-const tabContentItems = document.querySelectorAll('.tab-content-item');
+const tabContentItems = document.querySelectorAll('.tab__content--item');
 
 // Select tab content item
 function selectItem(e) {
@@ -26,40 +26,21 @@ function removeShow() {
 // Listen for tab click
 tabItems.forEach((item) => item.addEventListener('click', selectItem));
 
-// *TOGGLE
-const arrowToggle = document.querySelectorAll('.toggle');
 
-function toggleMe(e) {
-	console.log('Me');
-}
-
-arrowToggle.forEach((toggle) => {
-	toggle.addEventListener('click', toggleMe);
-});
-
-// Hamburger Menu 
+// Hamburger Menu
 const btnHamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
-// const fadeElements = document.querySelectorAll('.has-fade');
 const body = document.querySelector('body');
 
 btnHamburger.addEventListener('click', function () {
-
-  if (header.classList.contains('open')) { //Close hamburger menu
-    body.classList.remove('noscroll');
-    header.classList.remove('open');
-    // fadeElements.forEach(function (element) {
-    //   element.classList.remove('fade-in');
-    //   element.classList.add('fade-out');
-    // });
-
-  } else { //Open hamburger menu
-    body.classList.add('noscroll');
-    header.classList.add('open');
-    // fadeElements.forEach(function (element) {
-    //   element.classList.remove('fade-out');
-    //   element.classList.add('fade-in');
-    // });
-  }
+	if (header.classList.contains('open')) {
+		//Close hamburger menu
+		body.classList.remove('noscroll');
+		header.classList.remove('open');
+	} else {
+		//Open hamburger menu
+		body.classList.add('noscroll');
+		header.classList.add('open');
+	}
 });
