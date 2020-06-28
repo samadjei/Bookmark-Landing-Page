b22d985ef3d7ed4f3b9358ef9e83b70c91e84216
Samuel Adjei <samueladj3i@gmail.com>
2020-06-28 14:05:19 +0100

    adpated the svg icon to the hamburger background

--- js/app.js
+++ js/app.js
@@ -2,17 +2,28 @@
 const btnHamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
+const fadeElements = document.querySelectorAll('.has-fade');
 const body = document.querySelector('body');
 
-btnHamburger.addEventListener('click', () => {
+btnHamburger.addEventListener('click', function () {
+	console.log('click hamburger');
+
 	if (header.classList.contains('open')) {
 		//Close hamburger menu
 		body.classList.remove('noscroll');
 		header.classList.remove('open');
+		fadeElements.forEach(function (element) {
+			element.classList.remove('fade-in');
+			element.classList.add('fade-out');
+		});
 	} else {
 		//Open hamburger menu
 		body.classList.add('noscroll');
 		header.classList.add('open');
+		fadeElements.forEach(function (element) {
+			element.classList.remove('fade-out');
+			element.classList.add('fade-in');
+		});
 	}
 });
 
