b1ba3915211fc3f76db6ffb1976cdb07dec22f86
Samuel Adjei <samueladj3i@gmail.com>
2020-06-28 20:14:50 +0100

    adding some functionality to tabs

--- js/app.js
+++ js/app.js
@@ -28,30 +28,3 @@ btnHamburger.addEventListener('click', function () {
 });
 
 // *TABS
-const tabItems = document.querySelectorAll('.tab-item');
-const tabContentItems = document.querySelectorAll('.tab__content--item');
-
-// Select tab content item
-function selectItem(e) {
-	// Remove all show and border classes
-	removeBorder();
-	removeShow();
-	// Add border to current tab
-	this.classList.add('tab-border');
-	// Grab content item from the DOM
-	const tabContentItem = document.querySelector(`#${this.id}-content`);
-	// Add show class
-	tabContentItem.classList.add('show');
-}
-
-// Remove bottom borders from all tab items
-function removeBorder() {
-	tabItems.forEach((item) => item.classList.remove('tab-border'));
-}
-
-function removeShow() {
-	tabContentItems.forEach((item) => item.classList.remove('show'));
-}
-
-// Listen for tab click
-tabItems.forEach((item) => item.addEventListener('click', selectItem));
